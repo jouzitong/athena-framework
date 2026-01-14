c55724b2f3dfaaaddd5733864488b3e7e6b6b8a1
zhouzhitong <528382226@qq.com>
2026-01-14 23:11:27 +0800

    feat: 添加仿真任务功能及事件发布器改进
    - 在 `SpringEventPublisher` 中添加了日志记录和组件注解。
    - 为 `EmulateTask` 添加了新的索引，并移除了未使用的字段。
    - 新增 `EmulateTaskDTO` 类，用于传输仿真任务数据。
    - 将旧的 `TaskController` 和 `TaskControllerV2` 移动到 `old` 包下，并新增 `EmulateTaskControllerV2`。
    - 在 `IEmulateTaskService` 和 `EmulateTaskServiceImpl` 中添加了添加仿真任务的方法。
    - 新增 `NamingDTO` 和 `SettingConvertUtils` 工具类。
    - 修改了 `BaseEntity` 为已弃用状态。
    - 新增 `EmulateTaskAddEvent` 事件。

--- athena-project/athena-framework-common/src/main/java/org/arthena/framework/common/event/SpringEventPublisher.java
+++ athena-project/athena-framework-common/src/main/java/org/arthena/framework/common/event/SpringEventPublisher.java
@@ -1,22 +1,26 @@
 package org.arthena.framework.common.event;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
 
 /**
  * 事件发布器
  *
  * @author zhouzhitong
  */
-//@Component("")
-//@Deprecated
+@Component("springEventPublisher")
+@Slf4j
 public class SpringEventPublisher implements EventPublisher, ApplicationEventPublisherAware {
 
     private ApplicationEventPublisher applicationEventPublisher;
 
     @Override
     public void publish(Object event) {
+        LOGGER.debug("publish event: {}", event);
         applicationEventPublisher.publishEvent(event);
+        LOGGER.trace("publish finish.");
     }
 
     @Override
