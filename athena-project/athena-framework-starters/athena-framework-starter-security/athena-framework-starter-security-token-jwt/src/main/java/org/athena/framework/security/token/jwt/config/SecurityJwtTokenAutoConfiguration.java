608daaf19bed763c34a899738b4b4d524a09cfc4
zhouzhitong <528382226@qq.com>
2026-04-17 22:41:18 +0800

    fix: 优化jwt配置累

--- athena-project/athena-framework-starters/athena-framework-starter-security/athena-framework-starter-security-token-jwt/src/main/java/org/athena/framework/security/token/jwt/config/SecurityJwtTokenAutoConfiguration.java
+++ athena-project/athena-framework-starters/athena-framework-starter-security/athena-framework-starter-security-token-jwt/src/main/java/org/athena/framework/security/token/jwt/config/SecurityJwtTokenAutoConfiguration.java
@@ -2,12 +2,13 @@ package org.athena.framework.security.token.jwt.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.athena.framework.security.api.spi.TokenManager;
+import org.athena.framework.security.auth.core.config.SecurityAuthCoreAutoConfiguration;
 import org.athena.framework.security.starter.marker.SecurityCoreMarker;
 import org.athena.framework.security.token.jwt.service.JwtTokenManager;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
@@ -16,13 +17,12 @@ import org.springframework.context.annotation.Bean;
  * 在启用 JWT 且 token.type=jwt 时注册 {@link TokenManager} 实现。
  */
 @AutoConfiguration
+@AutoConfigureAfter(SecurityAuthCoreAutoConfiguration.class)
 @ConditionalOnBean(SecurityCoreMarker.class)
-@ConditionalOnProperty(prefix = "athena.security.token.jwt", name = "enabled", havingValue = "true")
 @EnableConfigurationProperties(JwtTokenProperties.class)
 public class SecurityJwtTokenAutoConfiguration {
 
     @Bean
-    @ConditionalOnProperty(prefix = "athena.security.token", name = "type", havingValue = "jwt")
     @ConditionalOnMissingBean(TokenManager.class)
     public TokenManager tokenManager(ObjectMapper objectMapper, JwtTokenProperties properties) {
         return new JwtTokenManager(objectMapper, properties);
